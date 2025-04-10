350031779713d07d88fa223890d118ef495bdf8f
laurenhill1 <laurenhill1@ufl.edu>
2025-04-10 11:58:35 -0400

    partner1 calc

--- calculator.py
+++ calculator.py
@@ -6,18 +6,32 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
+
 def add(a, b):
     return a + b
+
 def subtract(a, b):
     return a - b
+
 def multiply(a, b):
     return a * b
+
 def divide(a, b):
     if a == 0:
-        return "ZeroDivisionError"
-    else:
-        return a / b
-def
+        raise ZeroDivisionError("Cannot divide by zero.")
+    return b / a
+
+def logarithm(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Base must be greater than 0 and not equal to 1.")
+    if b <= 0:
+        raise ValueError("Argument must be greater than 0.")
+    return math.log(b, a)
+
+def exponent(a, b):
+    return a ** b
+
 
 
 
