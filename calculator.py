6adf9a5ea821198f647d8e44b247086ee7ef4fff
laurenhill1 <laurenhill1@ufl.edu>
2025-04-10 11:57:20 -0400

    calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -5,7 +6,18 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if a == 0:
+        return "ZeroDivisionError"
+    else:
+        return a / b
+def
+
 
 
