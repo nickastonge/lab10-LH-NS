a08d7ecb116b80ef77212daa0fa8fa1c0b7b995d
laurenhill1 <laurenhill1@ufl.edu>
2025-04-10 12:20:02 -0400

    help

--- calculator.py
+++ calculator.py
@@ -1,4 +1,3 @@
-import math
 """
 calculator.py
 - Defines functions used to create a simple calculator
@@ -47,31 +46,6 @@ def logarithm(a, b):
 
 def exponent(a, b):
     return a ** b
-
-def add(a, b):
-    return a + b
-
-def subtract(a, b):
-    return a - b
-
-def multiply(a, b):
-    return a * b
-
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError("Cannot divide by zero.")
-    return b / a
-
-def logarithm(a, b):
-    if a <= 0 or a == 1:
-        raise ValueError("Base must be greater than 0 and not equal to 1.")
-    if b <= 0:
-        raise ValueError("Argument must be greater than 0.")
-    return math.log(b, a)
-
-def exponent(a, b):
-    return a ** b
-
 
 
 
